d71eaee9f36b2e2c8825d512cf6b59dab3b3d346
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-02-10 20:38:31 +0530

    error handling using global error midleware

--- app.js
+++ app.js
@@ -14,4 +14,28 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
+//To handle unhandled routes
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Could not find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Could not find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+//global error handling middleware
+app.use((err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
