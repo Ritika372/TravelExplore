5e4eb10ba6f41cc029c6fe13bf8bbb67adeb1da2
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-02-11 10:03:29 +0530

    error handling refactor

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const globalErrorHandler = require('./Controllers/errorController');
+const appError = require('./utils/appError');
 const tourRouter = require('./Routes/tourRoutes');
 const userRouter = require('./Routes/userRoutes');
 const morgan = require('morgan');
@@ -16,26 +18,18 @@ app.use('/api/users', userRouter);
 
 //To handle unhandled routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Could not find ${req.originalUrl} on this server`,
-  // });
-  const err = new Error(`Could not find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Could not find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  const err = new appError(
+    `Could not find ${req.originalUrl} on this server`,
+    404
+  );
 
   next(err);
 });
 
 //global error handling middleware
-app.use((err, req, res, next) => {
-  err.status = err.status || 'error';
-  err.statusCode = err.statusCode || 500;
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
