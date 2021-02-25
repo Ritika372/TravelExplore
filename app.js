a48e8401b96f10f29c11a603da75971bbf58c6b5
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-02-25 20:35:17 +0530

    rate limiting

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const appError = require('./utils/appError');
 const tourRouter = require('./Routes/tourRoutes');
 const userRouter = require('./Routes/userRoutes');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again after an hour.',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
