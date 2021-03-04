33af8ab897fc25e60ac7797778aca946d763aa6c
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-03-04 17:01:56 +0530

    sanitization and http parameter pollution

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ const tourRouter = require('./Routes/tourRoutes');
 const userRouter = require('./Routes/userRoutes');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const MongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const app = express();
 
+//Set security http headers
+app.use(helmet());
+
+//deveelpmnet logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//to implement number of reuests a client can make to a server.
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -19,9 +29,33 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+//body parsisng, reading data from req.body
+app.use(express.json({ limit: '10kb' }));
+
+//data sanitization against nosql injections
+app.use(MongoSanitize());
+
+//data sanitization against xss
+app.use(xss());
+
+//prevent http parameter pollution.
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+//serve static files
 app.use(express.static(`${__dirname}/public`));
 
+//route handlers
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
